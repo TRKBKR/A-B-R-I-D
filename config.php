28774d46282b6a290df04775157f880239ed258d
ⵟⴰⵕⵉⴽ ⴱⴰⴽⵉⵕ <41262131+TRKBKR@users.noreply.github.com>
2020-06-29 21:04:47 +0100

    Add files via upload

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$user = "root";
+$pass = "toor";
+$db = 'test';
+// Create connection
+$conn = mysqli_connect($servername, $user, $pass, $db);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+function clean($str,$conn) {
+		$str = @trim($str);
+		if(get_magic_quotes_gpc()) {
+			$str = stripslashes($conn,$str);
+		}
+		return mysqli_real_escape_string($conn,$str);
+	}
+?>
+<!-- Tarik Bakir :\) --\>
